0e5897707a0f2d3db91770a0b8fbdd7ceb4a6706
Doug Morgan <doug.morgan@leandog.com>
2014-12-18 16:38:23 -0500

    Cleaned up spec assertions. New syntax.

--- spec/lib/soap_object_spec.rb
+++ spec/lib/soap_object_spec.rb
@@ -25,48 +25,48 @@ describe SoapObject do
 
 
   context "when creating new instances" do
-    before do 
-      Savon.should_receive(:client).and_return(client)
+    before do
+      allow(Savon).to receive(:client).and_return(client)
     end
   
     it "should initialize the client using the wsdl" do
-      subject.send(:client_properties)[:wsdl].should == 'http://blah.com'
+      expect(subject.send(:client_properties)[:wsdl]).to eq('http://blah.com')
     end
 
     it "should know when it is connected to service" do
-      subject.should be_connected
+      expect(subject).to be_connected
     end
 
     it "should allow one to setup a proxy" do
-      subject.send(:client_properties)[:proxy].should == 'http://proxy.com:8080'
+      expect(subject.send(:client_properties)[:proxy]).to eq('http://proxy.com:8080')
     end
 
     it "should allow one to set an open timeout" do
-      subject.send(:client_properties)[:open_timeout].should == 10
+      expect(subject.send(:client_properties)[:open_timeout]).to eq(10)
     end
 
     it "should allow one to set a read timeout" do
-      subject.send(:client_properties)[:read_timeout].should == 20
+      expect(subject.send(:client_properties)[:read_timeout]).to eq(20)
     end
 
     it "should allow one to set a soap header" do
-      subject.send(:client_properties)[:soap_header].should == {'Token' => 'secret'}
+      expect(subject.send(:client_properties)[:soap_header]).to eq({'Token' => 'secret'})
     end
 
     it "should allow one to set the encoding" do
-      subject.send(:client_properties)[:encoding].should == 'UTF-16'
+      expect(subject.send(:client_properties)[:encoding]).to eq('UTF-16')
     end
 
     it "should allow one to use basic authentication" do
-      subject.send(:client_properties)[:basic_auth].should == ['steve', 'secret']
+      expect(subject.send(:client_properties)[:basic_auth]).to eq(['steve', 'secret'])
     end
 
     it "should allow one to use digest authentication" do
-      subject.send(:client_properties)[:digest_auth].should == ['digest', 'auth']
+      expect(subject.send(:client_properties)[:digest_auth]).to eq(['digest', 'auth'])
     end
 
     it "should allow one to set the log level" do
-      subject.send(:client_properties)[:log_level].should == :error
+      expect(subject.send(:client_properties)[:log_level]).to eq(:error)
     end
   end
 
@@ -75,18 +75,18 @@ describe SoapObject do
 
     it "should create a valid service object" do
       service = world.using(TestServiceWithWsdl)
-      service.should be_instance_of TestServiceWithWsdl
+      expect(service).to be_instance_of TestServiceWithWsdl
     end
 
     it "should create a valid service and invoke a block" do
       world.using(TestServiceWithWsdl) do |service|
-        service.should be_instance_of TestServiceWithWsdl
+        expect(service).to be_instance_of TestServiceWithWsdl
       end
     end
 
     it "should create the service the first time we use it" do
       obj = TestServiceWithWsdl.new
-      TestServiceWithWsdl.should_receive(:new).once.and_return(obj)
+      expect(TestServiceWithWsdl).to receive(:new).once.and_return(obj)
       world.using(TestServiceWithWsdl)
       world.using(TestServiceWithWsdl)
     end
@@ -94,13 +94,13 @@ describe SoapObject do
 
   context "when calling methods on the service" do
     before do
-      Savon.should_receive(:client).and_return(client)
+      expect(Savon).to receive(:client).and_return(client)
     end
 
     it "should make a valid request" do
       response = double('response')
-      response.should_receive(:to_xml)
-      client.should_receive(:call).with(:fake_call, message: {data_key: 'some_value'}).and_return(response)
+      expect(response).to receive(:to_xml)
+      expect(client).to receive(:call).with(:fake_call, message: {data_key: 'some_value'}).and_return(response)
       @so = TestServiceWithWsdl.new
       @so.fake_call data_key: 'some_value'
     end
