1ea9e78245848209a4dc97f697d22c292c978631
Doug Morgan <doug.morgan@leandog.com>
2014-12-18 16:27:48 -0500

    Updated scenario expectations

--- features/step_definitions/basic_functionality_steps.rb
+++ features/step_definitions/basic_functionality_steps.rb
@@ -58,26 +58,26 @@ When /^I create an instance of the SoapObject class$/ do
 end
 
 Then /^I should have a connection$/ do
-  @so.should be_connected
+  expect(@so).to be_connected
 end
 
 Then /^I should be able to determine the operations$/ do
-  @so.operations.should include :get_info_by_zip
+  expect(@so.operations).to include :get_info_by_zip
 end
 
 Then /^I should be able to make a call and receive the correct results$/ do
   @so.get_zip_code_info(90210)
-  @so.state.should == 'CA'
-  @so.city.should == 'Beverly Hills'
-  @so.area_code.should == '310'
+  expect(@so.state).to eq('CA')
+  expect(@so.city).to  eq('Beverly Hills')
+  expect(@so.area_code).to eq('310')
 end
 
 Then /^the results xml should contain "(.*?)"$/ do |xml|
-  @so.to_xml.should include xml
+  expect(@so.to_xml).to include xml
 end
 
 Then /^the results doc should be a Nokogiri XML object$/ do
-  @so.doc.should be_instance_of Nokogiri::XML::Document
+  expect(@so.doc).to be_instance_of Nokogiri::XML::Document
 end
 
 Given /^I am calling the Define service$/ do
